29861fd0e5606cd83faa78b5c4d66b84a8ea7ff2
Jeffrey Rennie <rennie@google.com>
2020-08-25 13:51:44 -0700

    WIP

--- autosynth/providers/select_shard.py
+++ autosynth/providers/select_shard.py
@@ -15,35 +15,40 @@
 import importlib
 import sys
 from pprint import pprint
-from typing import Dict, List
+from typing import Dict, List, Any
+
+
+def shard_list(alist: List[Any], shard_count: int) -> List[List[Any]]:
+    """Breaks the list up into roughly-equally sized shards.
+
+    Args:
+        alist: A list of things.
+        shard_count (int): The total number of shards.
+
+    Returns:
+        List[List[Any]]: The shards.
+    """
+    shard_size = len(alist) / shard_count
+    shard_start = 0.0
+    for i in range(shard_count - 1):
+        shard_end = shard_start + shard_size
+        yield alist[int(shard_start):int(shard_end)]
+        shard_start = shard_end
+    yield alist[int(shard_start):]
 
 
 def main(args: List[str]):
     provider = args[1]
     shard_count = int(args[2])
+
     mod_name = "autosynth.providers." + provider
     mod = importlib.import_module(mod_name)
 
     print("Collecting list of repositories...")
     repos: List[Dict[str, str]] = mod.list_repositories()
-    
-    # Split the repos into equal-sized N shards.
     repos.sort(key=lambda repo: repo["name"])
-    shard_size, shard_mod = divmod(len(repos), shard_count)
-    shard_start = 0
-    shards = []
-    for i in range(shard_count):
-        if shard_mod > 0:
-            i_shard_size = shard_size + 1
-            shard_mod -= 1
-        else:
-            i_shard_size = shard_size
-        shard_end = shard_start + i_shard_size
-        shards.append(repos[shard_start:shard_end])
-        shard_start = shard_end  # Advance to next shard.
-    pprint(shards)
-    # Make sure we didn't misplace a repo while sharding.
-    assert len(repos) == sum([len(shard) for shard in shards])
+    shards = list(shard_list(repos, 10))
+    pprint(shards)    
 
 
 if __name__ == "__main__":
